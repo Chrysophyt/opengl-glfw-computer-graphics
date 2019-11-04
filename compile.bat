3d78b38e73bda76f5d5e3b88c3857776dd6828e4
Chrysophyt <ian240201@gmail.com>
2019-11-05 06:54:49 +0700

    done linking

--- compile.bat
+++ compile.bat
@@ -1,4 +1,7 @@
-g++ --version
-g++ -o "CompGraph.exe" "glew32.dll" "glfw3.dll" source.cpp -lopengl32 -lglu32 -lgdi32
+gcc --version
+gcc -o "CompGraph.o" source.cpp -static -libgcc -lopengl32 -lglu32 -lgdi32 
+gcc -o "CompGraph.exe" source.o "glew32.dll" "glfw3.dll" source.cpp -static-libgcc -lopengl32 -lglu32 -lgdi32 
+
+gcc -o "CompGraph.exe" "glew32.dll" "glfw3.dll" source.cpp -static-libgcc -lopengl32 -lglu32 -lgdi32 
 CompGraph
 pause

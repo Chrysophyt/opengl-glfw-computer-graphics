9d57750c34cc5708c71cfedead9e245d4e2f9c8f
Chrysophyt <ian240201@gmail.com>
2019-11-04 20:40:59 +0700

    add Example

--- compile.bat
+++ compile.bat
@@ -1,2 +1,4 @@
-gcc -o "CompGraph.exe" "glfw3.dll" source.cpp -lopengl32 -lglu32 -lgdi32 -static
+g++ --version
+g++ -o "CompGraph.exe" "glew32.dll" "glfw3.dll" source.cpp -lopengl32 -lglu32 -lgdi32
+CompGraph
 pause
--- source.cpp
+++ source.cpp
@@ -1,37 +1,127 @@
+#include "include/GL/glew.h"
 #include "include/GLFW/glfw3.h"
 
-int main(void)
+#include <cstdio>
+
+void controls(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    GLFWwindow* window;
+    if(action == GLFW_PRESS)
+        if(key == GLFW_KEY_ESCAPE)
+            glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Chrystian|18/430257/PA/18770", NULL, NULL);
-    if (!window)
+GLFWwindow* initWindow(const int resX, const int resY)
+{
+    if(!glfwInit())
     {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return NULL;
+    }
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(resX, resY, "TEST", NULL, NULL);
+
+    if(window == NULL)
+    {
+        fprintf(stderr, "Failed to open GLFW window.\n");
         glfwTerminate();
-        return -1;
+        return NULL;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, controls);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    // Get info of GPU and supported OpenGL version
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("OpenGL version supported %s\n", glGetString(GL_VERSION));
+
+    glEnable(GL_DEPTH_TEST); // Depth Testing
+    glDepthFunc(GL_LEQUAL);
+    glDisable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    return window;
+}
+
+void drawCube()
+{
+    GLfloat vertices[] =
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        -1, -1, -1,   -1, -1,  1,   -1,  1,  1,   -1,  1, -1,
+        1, -1, -1,    1, -1,  1,    1,  1,  1,    1,  1, -1,
+        -1, -1, -1,   -1, -1,  1,    1, -1,  1,    1, -1, -1,
+        -1,  1, -1,   -1,  1,  1,    1,  1,  1,    1,  1, -1,
+        -1, -1, -1,   -1,  1, -1,    1,  1, -1,    1, -1, -1,
+        -1, -1,  1,   -1,  1,  1,    1,  1,  1,    1, -1,  1
+    };
 
-        /* Swap front and back buffers */
+    GLfloat colors[] =
+    {
+        0, 0, 0,   0, 0, 1,   0, 1, 1,   0, 1, 0,
+        1, 0, 0,   1, 0, 1,   1, 1, 1,   1, 1, 0,
+        0, 0, 0,   0, 0, 1,   1, 0, 1,   1, 0, 0,
+        0, 1, 0,   0, 1, 1,   1, 1, 1,   1, 1, 0,
+        0, 0, 0,   0, 1, 0,   1, 1, 0,   1, 0, 0,
+        0, 0, 1,   0, 1, 1,   1, 1, 1,   1, 0, 1
+    };
+
+    static float alpha = 0;
+    //attempt to rotate cube
+    glRotatef(alpha, 0, 1, 0);
+
+    /* We have a color array and a vertex array */
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
+    glColorPointer(3, GL_FLOAT, 0, colors);
+
+    /* Send data : 24 vertices */
+    glDrawArrays(GL_QUADS, 0, 24);
+
+    /* Cleanup states */
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    alpha += 1;
+}
+
+void display( GLFWwindow* window )
+{
+    while(!glfwWindowShouldClose(window))
+    {
+        // Scale to window size
+        GLint windowWidth, windowHeight;
+        glfwGetWindowSize(window, &windowWidth, &windowHeight);
+        glViewport(0, 0, windowWidth, windowHeight);
+
+        // Draw stuff
+        glClearColor(0.0, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glMatrixMode(GL_PROJECTION_MATRIX);
+        glLoadIdentity();
+        gluPerspective( 60, (double)windowWidth / (double)windowHeight, 0.1, 100 );
+
+        glMatrixMode(GL_MODELVIEW_MATRIX);
+        glTranslatef(0,0,-5);
+
+        drawCube();
+
+        // Update Screen
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // Check for any input, or window movement
         glfwPollEvents();
     }
+}
 
+int main(int argc, char** argv)
+{
+    GLFWwindow* window = initWindow(1024, 620);
+    if( NULL != window )
+    {
+        display( window );
+    }
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
